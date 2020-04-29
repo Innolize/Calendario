1cbeb51f97547cba7f234a1de5e047e273a8d10e
Innolize <Innolize@users.noreply.github.com>
2020-04-28 21:58:14 -0300

    Comienza proyecto
    Creada tabla basica sin cuerpo

---
+++ main.js
@@ -0,0 +1,26 @@
+crearCalendario();
+function crearCalendario() {
+    ARRAY_DIAS = ["Domingo", "Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado"]
+
+    crearCalendarioHeaders(ARRAY_DIAS)
+
+    function crearCalendarioHeaders(ARRAY_DIAS) {
+        ARRAY_DIAS.forEach((DIA) => {
+            $dia = document.createElement("th")
+            $dia.textContent = DIA
+            document.querySelector("#dias-semana").appendChild($dia)
+        })
+    }
+    crearGrillaCalendario(ARRAY_DIAS)
+    function crearGrillaCalendario(ARRAY_DIAS) {
+        for (let i = 0; i < 4; i++) {
+            crearFila = document.createElement("tr")
+            ARRAY_DIAS.forEach((DIA) => {
+                nuevoCuadro = document.createElement("td")
+                nuevoCuadro.classList = `cuadro cuadro-${DIA}`
+                crearFila.appendChild(nuevoCuadro)
+            })
+            document.querySelector("#dias-calendario").appendChild(crearFila)
+        }
+    }
+}
