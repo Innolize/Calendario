69c10549de049f6925b54c9b10c1340901495d3a
Innolize <Innolize@users.noreply.github.com>
2020-05-02 05:05:28 -0300

    Separa calendarios semanal-mensual
    Comienza creando botones para seleccionar si ver calendario semanal o mensual

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
-creaBotonFDS();
-crearCalendario();
 
 
-function crearCalendario() {
+
+
+function crearCalendarioMensual() {
     ARRAY_DIAS = ["Domingo", "Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado"]
 
     crearCalendarioHeaders(ARRAY_DIAS)
@@ -28,9 +28,9 @@ function crearCalendario() {
         }
     }
 }
-numerarCalendario("Domingo", "30");
 
-function numerarCalendario(diaDeSemanaInicial, numeroDeDiasDelMes) {
+
+function numerarCalendarioMensual(diaDeSemanaInicial, numeroDeDiasDelMes) {
     let NUMERO_DE_CUADROS = 35
     let acumulador = 0
     obtenerCuadros = document.querySelectorAll(".cuadro")
@@ -54,7 +54,7 @@ function numerarCalendario(diaDeSemanaInicial, numeroDeDiasDelMes) {
             obtenerCuadros[i].classList.add("cuadro-deshabilitado")
         }
 }
-colocarIconoCrearEevento();
+
 function colocarIconoCrearEevento() {
     let dias = document.querySelectorAll(".cuadro")
     for (let i = 0; i < dias.length; i++) {
@@ -74,79 +74,81 @@ function colocarIconoCrearEevento() {
 
 //modal
 
-$(".agregar-evento").click((e) => {
-    diaNumero = e.target.parentElement.getAttribute("data-dia")
+function agregarEventoMensual() {
+    $(".agregar-evento").click((e) => {
+        diaNumero = e.target.parentElement.getAttribute("data-dia")
 
-    modalCrearEvento(diaNumero);
+        modalCrearEvento(diaNumero);
 
-    function modalCrearEvento(diaNumero) {
-        header = document.querySelector(".modal-title")
-        header.textContent = `Crear evento el dia ${diaNumero}`
+        function modalCrearEvento(diaNumero) {
+            header = document.querySelector(".modal-title")
+            header.textContent = `Crear evento el dia ${diaNumero}`
 
-        body = document.querySelector(".modal-body")
-        body.innerHTML = ""
+            body = document.querySelector(".modal-body")
+            body.innerHTML = ""
 
-        contenedorTitulo = document.createElement("div")
-        labelTitulo = document.createElement("label")
-        labelTitulo.textContent = "Nombre del evento:"
-        inputTitulo = document.createElement("input")
-        inputTitulo.id = "input-titulo"
-        contenedorTitulo.appendChild(labelTitulo)
-        contenedorTitulo.appendChild(inputTitulo)
+            contenedorTitulo = document.createElement("div")
+            labelTitulo = document.createElement("label")
+            labelTitulo.textContent = "Nombre del evento:"
+            inputTitulo = document.createElement("input")
+            inputTitulo.id = "input-titulo"
+            contenedorTitulo.appendChild(labelTitulo)
+            contenedorTitulo.appendChild(inputTitulo)
 
-        contenedorDescripcion = document.createElement("div")
-        labelDescripcion = document.createElement("label")
-        labelDescripcion.textContent = "Descripcion:"
-        inputDescripcion = document.createElement("input")
-        inputDescripcion.id = "input-descripcion"
-        contenedorDescripcion.appendChild(labelDescripcion)
-        contenedorDescripcion.appendChild(inputDescripcion)
+            contenedorDescripcion = document.createElement("div")
+            labelDescripcion = document.createElement("label")
+            labelDescripcion.textContent = "Descripcion:"
+            inputDescripcion = document.createElement("input")
+            inputDescripcion.id = "input-descripcion"
+            contenedorDescripcion.appendChild(labelDescripcion)
+            contenedorDescripcion.appendChild(inputDescripcion)
 
-        contenedorComienza = document.createElement("div")
-        labelComienza = document.createElement("label")
-        labelComienza.textContent = "Comienza:"
-        inputComienza = document.createElement("input")
-        inputComienza.id = "input-comienza"
-        inputComienza.type = "number"
-        inputComienza.setAttribute("placeholder", "HH:MM")
-        contenedorComienza.appendChild(labelComienza)
-        contenedorComienza.appendChild(inputComienza)
+            contenedorComienza = document.createElement("div")
+            labelComienza = document.createElement("label")
+            labelComienza.textContent = "Comienza:"
+            inputComienza = document.createElement("input")
+            inputComienza.id = "input-comienza"
+            inputComienza.type = "number"
+            inputComienza.setAttribute("placeholder", "HH:MM")
+            contenedorComienza.appendChild(labelComienza)
+            contenedorComienza.appendChild(inputComienza)
 
-        contenedorTermina = document.createElement("div")
-        labelTermina = document.createElement("label")
-        labelTermina.textContent = "Termina:"
-        inputTermina = document.createElement("input")
-        inputTermina.id = "input-termina"
-        inputTermina.type = "number"
-        inputTermina.setAttribute("placeholder", "HH:MM")
-        contenedorTermina.appendChild(labelTermina)
-        contenedorTermina.appendChild(inputTermina)
-
-
-
-        body.appendChild(contenedorTitulo)
-        body.appendChild(contenedorDescripcion)
-        body.appendChild(contenedorComienza)
-        body.appendChild(contenedorTermina)
+            contenedorTermina = document.createElement("div")
+            labelTermina = document.createElement("label")
+            labelTermina.textContent = "Termina:"
+            inputTermina = document.createElement("input")
+            inputTermina.id = "input-termina"
+            inputTermina.type = "number"
+            inputTermina.setAttribute("placeholder", "HH:MM")
+            contenedorTermina.appendChild(labelTermina)
+            contenedorTermina.appendChild(inputTermina)
 
 
-        footer = document.querySelector(".modal-footer")
-        footer.innerHTML = ""
-        crearBotonEvento("Cancelar", "secondary")
-        crearBotonEvento("Crear", "primary")
-        function crearBotonEvento(text, color) {
-            button = document.createElement("button")
-            button.type = "button"
-            button.textContent = text
-            button.classList = `btn btn-${color}`
-            button.dataset.dismiss = "modal"
-            footer.appendChild(button)
+
+            body.appendChild(contenedorTitulo)
+            body.appendChild(contenedorDescripcion)
+            body.appendChild(contenedorComienza)
+            body.appendChild(contenedorTermina)
+
+
+            footer = document.querySelector(".modal-footer")
+            footer.innerHTML = ""
+            crearBotonEvento("Cancelar", "secondary")
+            crearBotonEvento("Crear", "primary")
+            function crearBotonEvento(text, color) {
+                button = document.createElement("button")
+                button.type = "button"
+                button.textContent = text
+                button.classList = `btn btn-${color}`
+                button.dataset.dismiss = "modal"
+                footer.appendChild(button)
+
+            }
 
         }
 
-    }
-
-})
+    })
+}
 
 function creaBotonFDS() {
     boton = document.createElement("button")
@@ -154,34 +156,102 @@ function creaBotonFDS() {
     boton.textContent = "Test"
     document.querySelector("body").appendChild(boton)
 }
-$("#boton-fds").click(() => {
 
-    const filaDomingo = document.querySelectorAll(".cuadro-Domingo")
-    const filaSabado = document.querySelectorAll(".cuadro-Sabado")
-    const headerDomingo = document.querySelectorAll(".th-Domingo")
-    const headerSabado = document.querySelectorAll(".th-Sabado")
+function ocultarFDSMensual() {
+    $("#boton-fds").click(() => {
 
-    ocultarMostrarFDS(headerDomingo)
-    ocultarMostrarFDS(headerSabado)
-    ocultarMostrarFDS(filaDomingo)
-    ocultarMostrarFDS(filaSabado)
+        const filaDomingo = document.querySelectorAll(".cuadro-Domingo")
+        const filaSabado = document.querySelectorAll(".cuadro-Sabado")
+        const headerDomingo = document.querySelectorAll(".th-Domingo")
+        const headerSabado = document.querySelectorAll(".th-Sabado")
 
-    function ocultarMostrarFDS(elemento) {
-        elemento.forEach((dia) => {
-            debugger
-            if (dia.classList.contains("invisible") === true) {
-                dia.classList.remove("invisible")
-                dia.classList.add("visible")
-                return
-            } if (dia.classList.contains("invisible") === false) {
-                dia.classList.remove("visible")
-                dia.classList.add("invisible")
-                return
-            } else {
-                dia.classList.add("invisible")
-            }
+        ocultarMostrarFDS(headerDomingo)
+        ocultarMostrarFDS(headerSabado)
+        ocultarMostrarFDS(filaDomingo)
+        ocultarMostrarFDS(filaSabado)
+
+        function ocultarMostrarFDS(elemento) {
+            elemento.forEach((dia) => {
+                debugger
+                if (dia.classList.contains("invisible") === true) {
+                    dia.classList.remove("invisible")
+                    dia.classList.add("visible")
+                    return
+                } if (dia.classList.contains("invisible") === false) {
+                    dia.classList.remove("visible")
+                    dia.classList.add("invisible")
+                    return
+                } else {
+                    dia.classList.add("invisible")
+                }
 
 
+            })
+        }
+    })
+}
+
+
+crearBotonesTipoDeCalendario()
+function crearBotonesTipoDeCalendario() {
+
+    botonSemanal = document.createElement("button")
+    botonSemanal.id = "calendario-semanal"
+    botonSemanal.className = "btn btn-secondary"
+    botonSemanal.textContent = "Calendario Semanal"
+    document.querySelector("#tipo-calendario").appendChild(botonSemanal)
+
+    botonMensual = document.createElement("button")
+    botonMensual.id = "calendario-mensual"
+    botonMensual.className = "btn btn-secondary"
+    botonMensual.textContent = "Calendario Mensual"
+    document.querySelector("#tipo-calendario").appendChild(botonMensual)
+}
+
+$("#calendario-mensual").click(() => {
+    const trDiaSemanas = document.querySelector("#dias-semana")
+    const tbodyDias = document.querySelector("#dias-calendario")
+    borrarContenido(trDiaSemanas)
+    borrarContenido(tbodyDias)
+
+
+    creaBotonFDS();
+    crearCalendarioMensual();
+    numerarCalendarioMensual("Domingo", "30");
+    colocarIconoCrearEevento();
+    agregarEventoMensual()
+    ocultarFDSMensual()
+})
+
+function borrarContenido(elemento) {
+    elemento.innerHTML = ""
+}
+
+
+$("#calendario-semanal").click(() => {
+    const trDiaSemanas = document.querySelector("#dias-semana")
+    borrarContenido(trDiaSemanas)
+
+    ARRAY_DIAS = ["Horario", "Domingo", "Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado"]
+    crearCalendarioHeaders(ARRAY_DIAS)
+
+    function crearCalendarioHeaders(ARRAY_DIAS) {
+        ARRAY_DIAS.forEach((DIA) => {
+            $dia = document.createElement("th")
+            $dia.textContent = DIA
+            $dia.classList = `th-semanas th-${DIA}`
+            document.querySelector("#dias-semana").appendChild($dia)
         })
     }
+
+
+
+
+
+
+
+
+
+
+
 })
