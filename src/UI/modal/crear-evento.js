0877f38e78fd63a9a4cf609a19d2e61ffcad3222
Innolize <Innolize@users.noreply.github.com>
2020-05-06 11:23:34 -0300

    Crear eventos funcional
    Crear eventos deja datos en nuestra base de datos de manera correcta

--- src/UI/modal/crear-evento.js
+++ src/UI/modal/crear-evento.js
@@ -1,3 +1,5 @@
+import { fetchCrearEvento } from "../../service/manejador-eventos.js"
+
 export function modalCrearEvento() {
     debugger
     const header = document.querySelector(".modal-title")
@@ -31,8 +33,6 @@ export function modalCrearEvento() {
     const inputComienzaHora = document.createElement("input")
     inputComienzaHora.id = "crear-evento-comienza-hora"
     inputComienzaFecha.id = "crear-evento-comienza-fecha"
-    inputComienzaFecha.type = "number"
-    inputComienzaHora.type = "number"
     inputComienzaHora.setAttribute("placeholder", "HH:MM")
     inputComienzaFecha.setAttribute("placeholder", " ej: 20/5/2020")
     contenedorComienza.appendChild(labelComienzaFecha)
@@ -61,6 +61,7 @@ export function modalCrearEvento() {
     labelColor.textContent = "Selecciona color:"
     const inputColor = document.createElement("input")
     inputColor.setAttribute("placeholder", `ej: "red"o "#00FFFF"`)
+    inputColor.id = "crear-evento-color"
     contenedorColor.appendChild(labelColor)
     contenedorColor.appendChild(inputColor)
 
@@ -75,19 +76,57 @@ export function modalCrearEvento() {
     const footer = document.querySelector(".modal-footer")
     footer.innerHTML = ""
     crearBotonEvento("Cancelar", "secondary")
-    crearBotonEvento("Crear", "primary")
+    crearBotonEvento("crear", "primary")
     function crearBotonEvento(text, color) {
         const button = document.createElement("button")
         button.type = "button"
         button.textContent = text
+        button.id = text
         button.classList = `btn btn-${color}`
         button.dataset.dismiss = "modal"
         footer.appendChild(button)
-
-
-
-
-
     }
+
+
+
+
+    $("#crear").click(() => {
+        debugger
+        const nombreDelEvento = document.querySelector("#crear-evento-titulo").value
+        const descripcion = document.querySelector("#crear-evento-descripcion").value
+        const colorEvento = document.querySelector("#crear-evento-color").value
+        const comienzaFecha = document.querySelector("#crear-evento-comienza-fecha").value
+        const comienzaHora = document.querySelector("#crear-evento-comienza-hora").value
+        const terminaFecha = document.querySelector("#crear-evento-termina-fecha").value
+        const terminaHora = document.querySelector("#crear-evento-termina-hora").value
+        const comienza = obtenerFecha(comienzaFecha, comienzaHora)
+        const termina = obtenerFecha(terminaFecha, terminaHora)
+
+        function obtenerFecha(comienzaFecha, comienzaHora) {
+            let fecha = new Date(`${comienzaFecha} ${comienzaHora}`)
+            return fecha
+        }
+        debugger
+
+        let evento = {
+            created: new Date(),
+            updated: new Date(),
+            summary: nombreDelEvento,
+            description: descripcion,
+            color: colorEvento,
+            start: comienza,
+            end: termina,
+            creator: {
+                id: 1,
+                email: "test@test.com",
+                displayName: "Test Test",
+                self: true
+            }
+
+        }
+
+        fetchCrearEvento(evento)
+    })
+
     $("#modal").modal("show")
 }
